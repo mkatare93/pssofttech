48aa4e247907a1ce6084b8191252da7ae29dea86
Ajay <108121514+sharmajicodewale@users.noreply.github.com>
2022-12-16 12:53:37 +0530

    second push

---
+++ src/screen/FirstRoute.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const FirstRoute = () => {
+  return (
+    <View>
+      <Text>FirstRoute</Text>
+    </View>
+  )
+}
+
+export default FirstRoute
